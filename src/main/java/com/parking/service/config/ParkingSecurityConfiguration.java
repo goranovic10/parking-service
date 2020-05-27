44a6ebdf78a0e331eeb55f3848db45dbd02a7c7c
Nikola Goranovic <nikola.goranovicvg@gmail.com>
2020-05-27 13:04:04 +0200

    Create and deploy parking-service to GCP

---
+++ src/main/java/com/parking/service/config/ParkingSecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.parking.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ParkingSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+}
